027bec83591eef9a57c2b3e3550b8cd42d77f38d
Drew DeVault <sir@cmpwn.com>
2019-02-26 16:49:00 -0500

    Sideload wlr buffers into Godot GLES2 rasterizer

--- gles2_renderer.cpp
+++ gles2_renderer.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <wayland-server.h>
 #include <wlr/render/wlr_renderer.h>
 #include <wlr/render/interface.h>
+#include <wlr/util/log.h>
 #undef static
 
 static const enum wl_shm_format wl_formats[] = {
@@ -81,11 +82,38 @@ static bool renderer_format_supported(
 	return get_gles2_format_from_wl(fmt) != NULL;
 }
 
-static struct wlr_texture *renderer_texture_from_pixels(
-		struct wlr_renderer *_renderer, enum wl_shm_format fmt,
+struct wlr_texture *WlrGLES2Renderer::texture_from_pixels(
+		struct wlr_renderer *_renderer, enum wl_shm_format wl_fmt,
 		uint32_t stride, uint32_t width, uint32_t height, const void *data) {
-	WlrGLES2Renderer *renderer = (WlrGLES2Renderer *)_renderer;
-	return NULL;
+	struct WlrGLES2Renderer::renderer_state *state =
+		(struct WlrGLES2Renderer::renderer_state *)_renderer;
+	WlrGLES2Renderer *renderer = state->godot_renderer;
+	auto storage =
+		(RasterizerStorageGLES2 *)renderer->rasterizer->get_storage();
+	RID rid = storage->texture_create();
+	// TODO: Map shm formats to Image formats
+	storage->texture_allocate(rid, width, height, 0,
+			Image::FORMAT_RGB8, VS::TEXTURE_TYPE_2D,
+			VS::TEXTURE_FLAG_USED_FOR_STREAMING);
+	RasterizerStorageGLES2::Texture *texture =
+		storage->texture_owner.getornull(rid);
+
+	const struct gles2_pixel_format *fmt = get_gles2_format_from_wl(wl_fmt);
+	if (fmt == NULL) {
+		wlr_log(WLR_ERROR, "Unsupported pixel format %" PRIu32, wl_fmt);
+		return NULL;
+	}
+
+	glGenTextures(1, &texture->tex_id);
+	glBindTexture(GL_TEXTURE_2D, texture->tex_id);
+
+	glPixelStorei(GL_UNPACK_ROW_LENGTH_EXT, stride / (fmt->bpp / 8));
+	glTexImage2D(GL_TEXTURE_2D, 0, fmt->gl_format, width, height, 0,
+		fmt->gl_format, fmt->gl_type, data);
+	glPixelStorei(GL_UNPACK_ROW_LENGTH_EXT, 0);
+
+	WlrGLES2Texture *wlr_texture = new WlrGLES2Texture(rid, width, height);
+	return wlr_texture->get_wlr_texture();
 }
 
 static void renderer_init_wl_display(struct wlr_renderer *renderer,
@@ -140,22 +168,94 @@ static const struct wlr_renderer_impl renderer_impl = {
 	/* We use these */
 	.formats = renderer_formats,
 	.format_supported = renderer_format_supported,
-	.texture_from_pixels = renderer_texture_from_pixels,
+	.texture_from_pixels = WlrGLES2Renderer::texture_from_pixels,
 	.init_wl_display = renderer_init_wl_display,
 };
 
 }
 
 struct wlr_renderer *WlrGLES2Renderer::get_wlr_renderer() {
-	return &wlr_renderer;
+	return &renderer_state.wlr_renderer;
 }
 
 WlrGLES2Renderer::WlrGLES2Renderer(RasterizerGLES2 *p_rasterizer) {
 	rasterizer = p_rasterizer;
-	storage = dynamic_cast<RasterizerStorageGLES2 *>(rasterizer->get_storage());
-	wlr_renderer_init(&wlr_renderer, &renderer_impl);
+	wlr_renderer_init(&renderer_state.wlr_renderer, &renderer_impl);
+	renderer_state.godot_renderer = this;
+}
+
+extern "C" {
+
+void WlrGLES2Texture::wlr_texture_get_size(
+		struct wlr_texture *_texture, int *width, int *height) {
+	WlrGLES2Texture *texture = (WlrGLES2Texture *)_texture;
+	*width = texture->w;
+	*height = texture->h;
+}
+
+bool WlrGLES2Texture::wlr_texture_write_pixels(
+		struct wlr_texture *_texture, uint32_t stride,
+		uint32_t width, uint32_t height,
+		uint32_t src_x, uint32_t src_y, uint32_t dst_x, uint32_t dst_y,
+		const void *data) {
+	print_line("TODO: WlrGLES2Texture::texture_write_pixels");
+	return false;
+}
+
+void WlrGLES2Texture::wlr_texture_destroy(struct wlr_texture *texture) {
+	/* This space deliberately left blank */
+}
+
+static const struct wlr_texture_impl texture_impl = {
+	.get_size = WlrGLES2Texture::wlr_texture_get_size,
+	.write_pixels = WlrGLES2Texture::wlr_texture_write_pixels,
+	.destroy = WlrGLES2Texture::wlr_texture_destroy,
+};
+
 }
 
 WlrGLES2Renderer::~WlrGLES2Renderer() {
-	wlr_renderer_destroy(&wlr_renderer);
+	wlr_renderer_destroy(&renderer_state.wlr_renderer);
+}
+
+WlrGLES2Texture::WlrGLES2Texture(RID p_texture, int width, int height) {
+	wlr_texture_init(&wlr_texture, &texture_impl);
+	texture = p_texture;
+	w = width;
+	h = height;
+}
+
+int WlrGLES2Texture::get_width() const {
+	return w;
+}
+
+int WlrGLES2Texture::get_height() const {
+	return h;
+}
+
+RID WlrGLES2Texture::get_rid() const {
+	return texture;
+}
+
+bool WlrGLES2Texture::has_alpha() const {
+	// TODO
+	return true;
+}
+
+void WlrGLES2Texture::set_flags(uint32_t p_flags) {
+	flags = p_flags;
+	if (w == 0 || h == 0) {
+		return; //uninitialized, do not set to texture
+	}
+	// TODO: set flags
+	//VisualServer::get_singleton()->texture_set_flags(texture, p_flags);
+	_change_notify("flags");
+}
+
+uint32_t WlrGLES2Texture::get_flags() const {
+	return flags;
+}
+
+struct wlr_texture *WlrGLES2Texture::get_wlr_texture() {
+	return &wlr_texture;
 }
--- gles2_renderer.h
+++ gles2_renderer.h
@@ -1,19 +1,62 @@
 #ifndef GDWLR_GLES2_RENDERER_H
 #define GDWLR_GLES2_RENDERER_H
 #include "drivers/gles2/rasterizer_gles2.h"
+#include "scene/resources/texture.h"
 #include "renderer.h"
 extern "C" {
 #define static
 #include <wlr/render/wlr_renderer.h>
+#include <wlr/render/wlr_texture.h>
 #undef static
 }
 
-class WlrGLES2Renderer : public WlrRenderer {
-	struct wlr_renderer wlr_renderer;
-	RasterizerGLES2 *rasterizer;
-	RasterizerStorageGLES2 *storage;
+class WlrGLES2Texture : public Texture {
+	struct wlr_texture wlr_texture;
+
+	GDCLASS(WlrGLES2Texture, Texture);
+	RES_BASE_EXTENSION("wlrtex");
+
+	RID texture;
+	int w, h;
+	uint32_t flags;
 
 public:
+	struct wlr_texture *get_wlr_texture();
+
+	static void wlr_texture_get_size(
+			struct wlr_texture *_texture, int *width, int *height);
+	static bool wlr_texture_write_pixels(struct wlr_texture *_texture,
+			uint32_t stride, uint32_t width, uint32_t height,
+			uint32_t src_x, uint32_t src_y, uint32_t dst_x, uint32_t dst_y,
+			const void *data);
+	static void wlr_texture_destroy(struct wlr_texture *texture);
+
+	int get_width() const;
+	int get_height() const;
+	RID get_rid() const;
+	bool has_alpha() const;
+	uint32_t get_flags() const;
+	void set_flags(uint32_t p_flags);
+
+	WlrGLES2Texture(RID texture, int width, int height);
+};
+
+class WlrGLES2Renderer : public WlrRenderer {
+	RasterizerGLES2 *rasterizer;
+
+	/* Hack necessary for moving pointers between wlroots and godot */
+	struct renderer_state {
+		struct wlr_renderer wlr_renderer;
+		WlrGLES2Renderer *godot_renderer;
+	};
+
+	struct renderer_state renderer_state;
+
+public:
+	static struct wlr_texture *texture_from_pixels(
+			struct wlr_renderer *_renderer, enum wl_shm_format fmt,
+			uint32_t stride, uint32_t width, uint32_t height, const void *data);
+
 	virtual struct wlr_renderer *get_wlr_renderer();
 
 	WlrGLES2Renderer(RasterizerGLES2 *rasterizer);
