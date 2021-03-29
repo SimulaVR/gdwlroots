dd83d6c4e7ec8101eeee44bfbc807ebb34b53bf7
George Singer <george.w.singer@gmail.com>
2021-03-28 20:03:59 -0500

    Log to ./log/log.txt

--- debug.cpp
+++ debug.cpp
@@ -3,7 +3,7 @@
 
 void log_str(std::string str) {
 	std::ofstream log_file;
-	log_file.open("log.txt", std::fstream::app);
+	log_file.open("./log/log.txt", std::fstream::app);
 	log_file << str << std::endl;
 	log_file.close();
 }
